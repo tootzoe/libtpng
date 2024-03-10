00bb36563f38114d1f187731af845970b7232a81
toot_hzf <toot@tootzoe.com>
2024-03-10 15:13:53 +0700

    Initial Commit

---
+++ libtpng/libtpng.h
@@ -0,0 +1,24 @@
+//
+//  libtpng.h
+//  libtpng
+//
+//  Created by thor on 10/3/24
+//  
+//
+//  Email: toot@tootzoe.com  Tel: +855 69325538 
+//
+//
+
+
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for libtpng.
+FOUNDATION_EXPORT double libtpngVersionNumber;
+
+//! Project version string for libtpng.
+FOUNDATION_EXPORT const unsigned char libtpngVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libtpng/PublicHeader.h>
+
+
